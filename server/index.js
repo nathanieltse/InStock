609e8052dcbc9d2c1ca64a754787cd4c6e846d2a
Manjinder <smanjinder96.ms@gmail.com>
2021-06-28 13:49:58 -0700

    M21JH-29 Added Basic routes

--- server/index.js
+++ server/index.js
@@ -2,13 +2,23 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
+//routes goes here
 
 
+//env variables 
 require("dotenv").config();
 const port = process.env.PORT || 8080;
 
+//middleware
+app.use(express.json());
+app.use(express.static("public"))
+app.use(cors());
 
 
+//endoints 
+
+
+//listening on port 8080
 app.listen(port, () => {
     console.log(`App is running on port ${port}`);
 });
