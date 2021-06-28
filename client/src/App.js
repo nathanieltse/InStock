e6c73ae554e58c93526b846d480bf55aa2377c53
Okeke-miles <franklinokeke9@gmail.com>
2021-06-28 19:04:30 -0400

    pages and components folder fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,26 @@
 import './App.scss';
+import React, {Component} from "react";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import WarehouseList from "../../src/pages/WarehouseList";
+import WarehouseMainDisplay from "../../src/pages/WarehouseMainDisplay";
 
-function App() {
+
+
+class App extends Component{
+ 
+  render(){
+ 
   return (
-    <div className="App">
+      <BrowserRouter>
+        <Switch>
 
-      In Stock
+          <Route exact path="/" component={WarehouseList}/>
 
-    </div>
-  );
+          <Route path="/" component={WarehouseMainDisplay}/>
+
+        </Switch>
+    </BrowserRouter>
+  )}
 }
 
 export default App;
