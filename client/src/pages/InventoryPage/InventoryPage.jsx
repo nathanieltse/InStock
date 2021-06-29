7172df510791c44fc923c7828d1d2772ff840612
nathanieltse <nathanieltse@hotmail.com>
2021-06-28 23:08:00 -0400

    added header in inventory page

--- client/src/pages/InventoryPage/InventoryPage.jsx
+++ client/src/pages/InventoryPage/InventoryPage.jsx
@@ -1,12 +1,13 @@
 import { Component } from 'react';
+import PageHeader from '../../components/PageHeader/PageHeader'
 import './InventoryPage.scss'
 
 class InventoryPage extends Component {
     render () {
         return (
-            <div>
-
-            </div>
+            <section>
+                <PageHeader/>
+            </section>
         )
     }
 }
