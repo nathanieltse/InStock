83c0a61fbe56b74cf60f3b47555a461974401ed5
nathanieltse <nathanieltse@hotmail.com>
2021-07-01 17:27:31 -0400

    seperated search header and fixed styling

--- client/src/components/Listing/Listing.jsx
+++ client/src/components/Listing/Listing.jsx
@@ -1,31 +1,14 @@
 import ListingCard from "../ListingCard/ListingCard"
 import Labels from "../Labels/Labels"
-import { Link } from "react-router-dom"
+import SearchHeader from "../SearchHeader/SearchHeader"
+
 
 import "./Listing.scss";
 
 function Listing({warehouseList}) {
     return (
         <section className="listing">
-            <section className="listing__header">
-                <h1 className="listing__title">Warehouses</h1>
-                <form className="listing__form">
-                    <input
-                        className="listing__form-input listing__form-input--search"
-                        type="text"
-                        name="search"
-                        placeholder="Search..."
-                    />
-                    <Link to="/warehouses/add">
-                    <input
-                        className="listing__form-input listing__form-input--submit"
-                        type="submit"
-                        name="addWarehouse"
-                        value="+ Add New Warehouse"
-                        />
-                    </Link>
-                </form>
-            </section>
+            <SearchHeader/>
             <section className="listing__labels-container">
                 <Labels  name="WAREHOUSE" />
                 <Labels  name="ADDRESS" />
