84d6459d2ef0e2484db20b33f2bb2580b9cbf0c3
nathanieltse <nathanieltse@hotmail.com>
2021-07-01 00:59:03 -0400

    fixed class name

--- client/src/components/WarehouseListCard/WarehouseListCard.jsx
+++ client/src/components/WarehouseListCard/WarehouseListCard.jsx
@@ -7,28 +7,28 @@ import { Link } from "react-router-dom";
 function WarehouseListCard({ warehouse }) {
     return (
         <>
-            <div className="card__list" key={warehouse.id}>
-                <section className="card__listItem">
-                    <h4 className="card__listItem-title">WAREHOUSE</h4>
-                    <h3 className="card__listItem-text warehouse__listItem-text--name">{warehouse.name}
+            <div className="warehouse-list-card__list" key={warehouse.id}>
+                <section className="warehouse-list-card__listItem">
+                    <h4 className="warehouse-list-card__listItem-title">WAREHOUSE</h4>
+                    <h3 className="warehouse-list-card__listItem-text warehouse__listItem-text--name">{warehouse.name}
                         <Link to={`/warehouses/${warehouse.id}/detail`} >
                             <img className="warehouse__listItem-text--img" src={chevron} alt="expand warehouse detail"/>
                         </Link>
                     </h3>
                 </section>
-                <section className="card__listItem warehouse__listItem-address">
-                    <h4 className="card__listItem-title">ADDRESS</h4>
-                    <p className="card__listItem-text">{warehouse.address} ,</p>
-                    <p className="card__listItem-text" >{warehouse.city}, {warehouse.country}</p>
+                <section className="warehouse-list-card__listItem warehouse__listItem-address">
+                    <h4 className="warehouse-list-card__listItem-title">ADDRESS</h4>
+                    <p className="warehouse-list-card__listItem-text">{warehouse.address} ,</p>
+                    <p className="warehouse-list-card__listItem-text" >{warehouse.city}, {warehouse.country}</p>
                 </section>
-                <section className="card__listItem">
-                    <h4 className="card__listItem-title">CONTACT NAME</h4>
-                    <p className="card__listItem-text">{warehouse.contact.name}</p>
+                <section className="warehouse-list-card__listItem">
+                    <h4 className="warehouse-list-card__listItem-title">CONTACT NAME</h4>
+                    <p className="warehouse-list-card__listItem-text">{warehouse.contact.name}</p>
                 </section>
-                <section className="card__listItem">
-                    <h4 className="card__listItem-title">WAREHOUSE</h4>
-                    <p className="card__listItem-text">{warehouse.contact.phone}</p>
-                    <p className="card__listItem-text">{warehouse.contact.email}</p>
+                <section className="warehouse-list-card__listItem">
+                    <h4 className="warehouse-list-card__listItem-title">WAREHOUSE</h4>
+                    <p className="warehouse-list-card__listItem-text">{warehouse.contact.phone}</p>
+                    <p className="warehouse-list-card__listItem-text">{warehouse.contact.email}</p>
                 </section>
             </div>
             <Actions path={warehouse.id}/>
