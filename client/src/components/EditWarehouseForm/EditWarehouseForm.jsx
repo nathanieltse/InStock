6981405f37fdd285b166263e22e5e5191daa0289
nathanieltse <nathanieltse@hotmail.com>
2021-06-30 21:01:06 -0400

    added warning labels

--- client/src/components/EditWarehouseForm/EditWarehouseForm.jsx
+++ client/src/components/EditWarehouseForm/EditWarehouseForm.jsx
@@ -1,40 +1,73 @@
 import { Component } from 'react'
 import MainHeader from '../MainHeader/MainHeader'
+import errorIcon from '../../assets/Icons/error-24px.svg'
 import './EditWarehouseForm.scss'
 
 class EditWarehouseForm extends Component {
 
     render(){
         return (
-                <form className="warehouseform">
+                <form className="editwarehouseform">
                     <MainHeader navigate={this.props} headerName="Edit Form"/>
-                    <div className="warehouseform__wrapper">
-                        <div className="warehouseform__left">
-                            <h2 className="warehouseform__title">Warehouse Details</h2>
-                            <label htmlFor="name" className="warehouseform__label">WarehouseName</label>
-                            <input placeholder="Warehouse Name" name="name" className="warehouseform__input"></input>
-                            <label htmlFor="address" className="warehouseform__label">Street Address</label>
-                            <input placeholder="Street Address" name="address" className="warehouseform__input"></input>
-                            <label htmlFor="city" className="warehouseform__label">City</label>
-                            <input placeholder="City" name="city" className="warehouseform__input"></input>
-                            <label htmlFor="country" className="warehouseform__label">Country</label>
-                            <input placeholder="Country" className="warehouseform__input"></input>
+                    <div className="editwarehouseform__wrapper">
+                        <div className="editwarehouseform__left">
+                            <h2 className="editwarehouseform__title">Warehouse Details</h2>
+                            <label htmlFor="name" className="editwarehouseform__label">WarehouseName</label>
+                            <input placeholder="Warehouse Name" name="name" className="editwarehouseform__input"></input>
+                            <div className="editwarehouseform__warning">
+                                <img className="editwarehouseform__warning-icon" src={errorIcon} alt="error icon"/>
+                                <p className="editwarehouseform__warning-text">This field is required</p>
+                            </div>
+                            <label htmlFor="address" className="editwarehouseform__label">Street Address</label>
+                            <input placeholder="Street Address" name="address" className="editwarehouseform__input"></input>
+                            <div className="editwarehouseform__warning">
+                                <img className="editwarehouseform__warning-icon" src={errorIcon} alt="error icon"/>
+                                <p className="editwarehouseform__warning-text">This field is required</p>
+                            </div>
+                            <label htmlFor="city" className="editwarehouseform__label">City</label>
+                            <input placeholder="City" name="city" className="editwarehouseform__input"></input>
+                            <div className="editwarehouseform__warning">
+                                <img className="editwarehouseform__warning-icon" src={errorIcon} alt="error icon"/>
+                                <p className="editwarehouseform__warning-text">This field is required</p>
+                            </div>
+                            <label htmlFor="country" className="editwarehouseform__label">Country</label>
+                            <input placeholder="Country" className="editwarehouseform__input"></input>
+                            <div className="editwarehouseform__warning">
+                                <img className="editwarehouseform__warning-icon" src={errorIcon} alt="error icon"/>
+                                <p className="editwarehouseform__warning-text">This field is required</p>
+                            </div>
                         </div>
-                        <div className="warehouseform__right">
-                            <h2 className="warehouseform__title">Contact Details</h2>
-                            <label htmlFor="contactName" className="warehouseform__label">Contact Name</label>
-                            <input placeholder="Contact Name" name="contactName" className="warehouseform__input"></input>
-                            <label htmlFor="position" className="warehouseform__label">Position</label>
-                            <input placeholder="Position" name="position" className="warehouseform__input"></input>
-                            <label htmlFor="number" className="warehouseform__label">Phone Number</label>
-                            <input placeholder="Phone Number" name="number" className="warehouseform__input"></input>
-                            <label htmlFor="email" className="warehouseform__label">Email</label>
-                            <input placeholder="Email" name="email" className="warehouseform__input"></input>
+                        <div className="editwarehouseform__right">
+                            <h2 className="editwarehouseform__title">Contact Details</h2>
+                            <label htmlFor="contactName" className="editwarehouseform__label">Contact Name</label>
+                            <input placeholder="Contact Name" name="contactName" className="editwarehouseform__input"></input>
+                            <div className="editwarehouseform__warning">
+                                <img className="editwarehouseform__warning-icon" src={errorIcon} alt="error icon"/>
+                                <p className="editwarehouseform__warning-text">This field is required</p>
+                            </div>
+                            <label htmlFor="position" className="editwarehouseform__label">Position</label>
+                            <input placeholder="Position" name="position" className="editwarehouseform__input"></input>
+                            <div className="editwarehouseform__warning">
+                                <img className="editwarehouseform__warning-icon" src={errorIcon} alt="error icon"/>
+                                <p className="editwarehouseform__warning-text">This field is required</p>
+                            </div>
+                            <label htmlFor="number" className="editwarehouseform__label">Phone Number</label>
+                            <input placeholder="Phone Number" name="number" className="editwarehouseform__input"></input>
+                            <div className="editwarehouseform__warning">
+                                <img className="editwarehouseform__warning-icon" src={errorIcon} alt="error icon"/>
+                                <p className="editwarehouseform__warning-text">This field is required</p>
+                            </div>
+                            <label htmlFor="email" className="editwarehouseform__label">Email</label>
+                            <input placeholder="Email" name="email" className="editwarehouseform__input"></input>
+                            <div className="editwarehouseform__warning">
+                                <img className="editwarehouseform__warning-icon" src={errorIcon} alt="error icon"/>
+                                <p className="editwarehouseform__warning-text">This field is required</p>
+                            </div>
                         </div>
                     </div>
-                    <div className="warehouseform__action">
-                        <button type="submit" className="warehouseform__cancel">Cancel</button>
-                        <button type="submit" className="warehouseform__submit">Save</button>
+                    <div className="editwarehouseform__action">
+                        <button type="submit" className="editwarehouseform__cancel">Cancel</button>
+                        <button type="submit" className="editwarehouseform__submit">Save</button>
                     </div>
                 </form>
         )
