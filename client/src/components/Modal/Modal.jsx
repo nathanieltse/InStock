d234511e4907751a5e1e38b5d1add7e3e013a4b3
nathanieltse <nathanieltse@hotmail.com>
2021-07-05 19:39:21 -0400

    style fixes

--- client/src/components/Modal/Modal.jsx
+++ client/src/components/Modal/Modal.jsx
@@ -38,17 +38,20 @@ const Modal = ({ currentWarehouse, currentInventory, hideModal, displayModal, de
 
       <>
         <div className="modal display-block">
-            <section className='modal-main'>
-                <img className="modal-image__style" src={closeIcon} alt="cancel" onClick={hideModal}/>
-                <div className="modal-container__style">
-                    <h1 className="modal-title__style">Delete {currentInventory.itemName} inventory item?</h1>
-                    <p className="modal-body__style">Please confirm that you'd like to delete the {currentInventory.itemName} from the inventory list. You won't be able to undo this action.</p>
-                    <div className="modal-button__container">                        
-                        <button className="delete-button__style" onClick={()=>deleteInventory(currentInventory.id)}>
-                            Delete
-                        </button>
-                        <button className="cancel-button__style" onClick={hideModal}>
+            <section className='modal__main'>
+                <div className="modal__container">
+                    <img className="modal__cancel-icon" src={closeIcon} alt="cancel" onClick={hideModal}/>
+                    <div className="modal__container-text">
+                        <h1 className="modal__title">Delete {currentInventory.itemName} inventory item?</h1>
+                        <p className="modal__body">Please confirm that you'd like to delete the {currentInventory.itemName} from the inventory list. You won't be able to undo this action.</p>
+                    </div>
+
+                    <div className="modal__button-container">              
+                        <button className="cancel__button" onClick={hideModal}>
                             Cancel
+                        </button>          
+                        <button className="delete__button" onClick={()=>deleteInventory(currentInventory.id)}>
+                            Delete
                         </button>
                     </div>
                 </div>
